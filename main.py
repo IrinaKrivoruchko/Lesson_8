1c1a822315fddd7e06bce27e4525c13618a09166
Ira <sokurira@gmail.com>
2023-10-17 23:49:58 +0300

    Phone number

--- main.py
+++ main.py
@@ -10,3 +10,17 @@ for home_number in home_numbers:
         print(f"{home_number} - Right")
     else:
         print(f"{home_number} - Not right")
+
+#2 Phone number
+pattern = r'^(?:\+?380|\b380)\d{9}$'
+
+# Примеры телефонных номеров для проверки
+phone_numbers = ["+380123456789", "380987654321", "014785236", "+380j00000000"]
+
+for phone_number in phone_numbers:
+    if re.match(pattern, phone_number):
+        print(f"{phone_number} - Right")
+    else:
+        print(f"{phone_number} - Not right")
+
+
