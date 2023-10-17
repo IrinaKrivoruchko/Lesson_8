5806583c3c20634b0ce438ff773e88a18af79838
Ira <sokurira@gmail.com>
2023-10-18 00:12:43 +0300

    Client's name

--- main.py
+++ main.py
@@ -24,13 +24,26 @@ import re
 #         print(f"{phone_number} - Not right")
 
 #3 email
-pattern = r'^[a-zA-Z0-9._%+-]{3,30}@[a-zA-Z0-9.-]{2,10}\.[a-zA-Z]{2,5}$'
+# pattern = r'^[a-zA-Z0-9._%+-]{3,30}@[a-zA-Z0-9.-]{2,10}\.[a-zA-Z]{2,5}$'
+#
+# # Примеры email-адресов для проверки
+# emails = ["Kryvoruchko@gmail.com", "Kryvoruchko_Iryna@domain.co.uk", "a@b.c", "invalid.email@qwertyuiopl.asdfghjkl"]
+#
+# for email in emails:
+#     if re.match(pattern, email):
+#         print(f"{email} - Right")
+#     else:
+#         print(f"{email} - Not right")
 
-# Примеры email-адресов для проверки
-emails = ["Kryvoruchko@gmail.com", "Kryvoruchko_Iryna@domain.co.uk", "a@b.c", "invalid.email@qwertyuiopl.asdfghjkl"]
+#4
+pattern = r'^[а-яА-Яa-zA-ZіІїЇєЄґҐ]{2,20}\s[а-яА-Яa-zA-ZіІїЇєЄґҐ]{2,20}\s[а-яА-Яa-zA-ZіІїЇєЄґҐ]{2,20}$'
 
-for email in emails:
-    if re.match(pattern, email):
-        print(f"{email} - Right")
+# Примеры ФИО для проверки
+full_names = ["Криворучко Ірина Валеріївна", "Иванов Иван Иванович", "Иванов Иван", "John", "qwertyuioplkjhgfdsazxcvbnm"]
+
+for full_name in full_names:
+    if re.match(pattern, full_name):
+        print(f"{full_name} - Right")
     else:
-        print(f"{email} - Not right")
+        print(f"{full_name} - Not right")
+
