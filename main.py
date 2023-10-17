38c06113b94949921b907cef4d05656dd3b2b9a7
Ira <sokurira@gmail.com>
2023-10-18 00:01:59 +0300

    email

--- main.py
+++ main.py
@@ -1,26 +1,36 @@
 import re
 #1 Home number
-pattern = r'^\d{9}$'
-
-# Примеры телефонных номеров для проверки
-home_numbers = ["123456789", "987654321", "12345ABC", "1234567890"]
-
-for home_number in home_numbers:
-    if re.match(pattern, home_number):
-        print(f"{home_number} - Right")
-    else:
-        print(f"{home_number} - Not right")
+# pattern = r'^\d{9}$'
+#
+# # Примеры телефонных номеров для проверки
+# home_numbers = ["123456789", "987654321", "12345ABC", "1234567890"]
+#
+# for home_number in home_numbers:
+#     if re.match(pattern, home_number):
+#         print(f"{home_number} - Right")
+#     else:
+#         print(f"{home_number} - Not right")
 
 #2 Phone number
-pattern = r'^(?:\+?380|\b380)\d{9}$'
+# pattern = r'^(?:\+?380|\b380)\d{9}$'
+#
+# # Примеры телефонных номеров для проверки
+# phone_numbers = ["+380123456789", "380987654321", "014785236", "+380j00000000"]
+#
+# for phone_number in phone_numbers:
+#     if re.match(pattern, phone_number):
+#         print(f"{phone_number} - Right")
+#     else:
+#         print(f"{phone_number} - Not right")
 
-# Примеры телефонных номеров для проверки
-phone_numbers = ["+380123456789", "380987654321", "014785236", "+380j00000000"]
+#3 email
+pattern = r'^[a-zA-Z0-9._%+-]{3,30}@[a-zA-Z0-9.-]{2,10}\.[a-zA-Z]{2,5}$'
 
-for phone_number in phone_numbers:
-    if re.match(pattern, phone_number):
-        print(f"{phone_number} - Right")
+# Примеры email-адресов для проверки
+emails = ["Kryvoruchko@gmail.com", "Kryvoruchko_Iryna@domain.co.uk", "a@b.c", "invalid.email@qwertyuiopl.asdfghjkl"]
+
+for email in emails:
+    if re.match(pattern, email):
+        print(f"{email} - Right")
     else:
-        print(f"{phone_number} - Not right")
-
-
+        print(f"{email} - Not right")
